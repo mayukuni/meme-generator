260a625505d4913d0d34d2726261d66b926a79eb
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-10-21 23:48:26 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const input = document.getElementById('text-input');
 // const container = document.getElementById('meme-image-container');
 const memeText = document.getElementById('meme-text');
+const fileInput = document.getElementById('meme-insert');
+const memeImage = document.getElementById('meme-image');
 
 // 1
 function showInputValue() {
@@ -9,3 +11,16 @@ function showInputValue() {
 }
 
 input.addEventListener('keyup', showInputValue);
+
+// 2
+// https://www.horadecodar.com.br/2020/05/20/javascript-preview-de-imagem-carregada-em-input-file/
+function insertImage() {
+  if (this.files && this.files[0]) {
+    const file = new FileReader();
+    file.onload = (event) => {
+      memeImage.src = event.target.result;
+    };
+    file.readAsDataURL(this.files[0]);
+  }
+}
+fileInput.addEventListener('change', insertImage, false);
