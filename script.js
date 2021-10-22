bc8c40dfb490c85bbf1d6847da3ec451477fd721
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-10-22 00:20:03 -0300

    requisito 4

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ input.addEventListener('keyup', showInputValue);
 
 // 2
 // https://www.horadecodar.com.br/2020/05/20/javascript-preview-de-imagem-carregada-em-input-file/
-function insertImage(event) {
-  const file = new FileReader();
-  file.onload = function addImage() {
-    memeImage.src = file.result;
-  };
-  file.readAsDataURL(event.target.files[0]);
-}
-fileInput.addEventListener('change', insertImage, true);
+// function insertImage(event) {
+//   const file = new FileReader();
+//   file.onload = function addImage() {
+//     memeImage.src = file.result;
+//   };
+//   file.readAsDataURL(event.target.files[0]);
+// }
+// fileInput.addEventListener('change', insertImage, true);
